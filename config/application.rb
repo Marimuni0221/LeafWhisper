cede9b9b434d621563d567fc60019481f8581a75
Marie Suzuki <Marie@MacBook-Air.local>
2024-07-12 23:06:04 +0900

    deviseの日本語化を行いました

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ module LeafWhisper
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-
+    
+    # deviseの日本語化設定
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
