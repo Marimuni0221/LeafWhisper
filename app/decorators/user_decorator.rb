596e20fc6602a3be415afb8101a71e9f5f9a1a69
Marie Suzuki <Marie@MacBook-Air.local>
2024-07-19 23:19:17 +0900

    プロフィール編集画面の設定を行いました

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,5 +4,9 @@ class UserDecorator < Draper::Decorator
   def display_name
     object.name.present? ? object.name : '匿名ユーザー'
   end
+  
 
+  def display_avatar_url
+    object.avatar.present? ? object.avatar.url : ActionController::Base.helpers.asset_path('default_avatar.png')
+  end
 end
