d0d81b3677a61c613a67f63a3f0ff4b17d8773c9
Marie Suzuki <Marie@MacBook-Air.local>
2024-07-22 18:49:07 +0900

    編集画面の設定とアバターアップロード機能の確認と編集を行 いました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
   
   def current_user
     super&.decorate
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
+  end
+
+  private
+
+  def set_search
+    @q = Product.ransack(params[:q])    
   end
 end
