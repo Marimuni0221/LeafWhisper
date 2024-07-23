f848286da1405fc30bc94ea291cc9f1892aea07e
Marie Suzuki <Marie@MacBook-Air.local>
2024-07-23 21:30:10 +0900

    item_codeのカラムをProductテーブルに追加しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_20_140349) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_083410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_20_140349) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.integer "category", default: 0, null: false
+    t.string "item_code"
+    t.index ["item_code"], name: "index_products_on_item_code", unique: true
   end
 
   create_table "reviews", force: :cascade do |t|
