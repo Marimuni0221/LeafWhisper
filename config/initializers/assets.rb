05a58a8ce721bd5f0a42a56e640bf1484cb65d40
Marie Suzuki <Marie@MacBook-Air.local>
2024-07-28 21:42:26 +0900

    編集しました

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,8 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'images', 'builds')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( matcha_powder.jpg )
+
