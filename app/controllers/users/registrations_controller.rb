b0e03067824994462766f5f35e1e30678f02c69a
Marie Suzuki <Marie@MacBook-Air.local>
2024-08-06 18:48:52 +0900

    google認証の際にパスワード入力なしでプロフィール編集が出来るようにしました

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  
   # GET /resource/sign_up
   # def new
   #   super
@@ -40,6 +40,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # protected
 
+  def update_resource(resource, params)
+    if resource.provider == 'google_oauth2'
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
