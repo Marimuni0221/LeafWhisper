e8b1501d31bebf1404af6fba0d85a3be12bde195
Marie Suzuki <Marie@MacBook-Air.local>
2024-08-18 09:36:50 +0900

    お問い合わせメール送信機能に関するテストを追加しました

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,17 @@ class ContactsController < ApplicationController
     contact_email = params[:email]
     contact_message = params[:message]
 
+    # バリデーションの実施
+    if contact_name.blank? || contact_email.blank? || contact_message.blank?
+      flash.now[:alert] = []
+      flash.now[:alert] << "名前を入力してください" if contact_name.blank?
+      flash.now[:alert] << "メールアドレスを入力してください" if contact_email.blank?
+      flash.now[:alert] << "メッセージを入力してください" if contact_message.blank?
+      
+      render :new, status: :unprocessable_entity
+      return
+    end
+
     # メールを送信
     ContactMailer.send_contact_email(contact_name, contact_email, contact_message).deliver_now
 
