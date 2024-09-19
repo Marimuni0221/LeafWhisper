4fedee8b6a9e30878485384903a537aff8ee5efb
Marie Suzuki <Marie@MacBook-Air.local>
2024-09-19 11:43:03 +0900

    マイグレーションファイルのコメントを削除しました

--- db/migrate/20240905093956_add_unique_index_to_products_url_hash.rb
+++ db/migrate/20240905093956_add_unique_index_to_products_url_hash.rb
@@ -2,11 +2,9 @@
 
 class AddUniqueIndexToProductsUrlHash < ActiveRecord::Migration[7.1]
   def change
-    # 既存のインデックスを削除（もし存在する場合）
     remove_index :products, :url_hash if index_exists?(:products, :url_hash)
     remove_index :products, :item_url if index_exists?(:products, :item_url)
 
-    # ユニークインデックスを追加
     add_index :products, :url_hash, unique: true
     add_index :products, :item_url, unique: true
   end
